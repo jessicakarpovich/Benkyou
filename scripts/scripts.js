2f74b0f84a7759d67b93dc84683fbdcd8586835b
Jessica Karpovich <karpovichjm@gmail.com>
2018-02-16 19:44:34 -0800

    Cleaned up showStrokeOrder function.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -60,9 +60,9 @@ menuIcon.addEventListener('click', expandMenu, false);
 
 
 
-/**********  *********/
-    /*Stroke Order - Kanji Page*/
-/*********   *********/
+/******************  *****************/
+    /* Kanji Page - Kanji Alive API */
+/*****************   *****************/
 
 // Key from mashape https://market.mashape.com/JessicaKarpovich/applications/Kanji-Search
 const key = "3SIUgT55Vxmsht3cVhbNhsPpXEPtp1NZQ84jsn24FyCRXTvJEB";
@@ -80,39 +80,53 @@ myHeaders.append("Accept", "application/json");
 const myInit = { headers: myHeaders };
 
 
+
+/**********  *********/
+    /*Stroke Order - Numbers 1-10*/
+/*********   *********/
+
+/* Array numbers 1-10 kanji*/
 const numberArray = ["一",　"二",　"三",　"四",　"五",　"六",　"七",　"八",　"九",　"十"];
+/* Array (js-1 to js-10) one for each kanji in numberArray */
 const divArray = ["js-1", "js-2", "js-3", "js-4", "js-5", "js-6", "js-7", "js-8", "js-9", "js-10"];
 
 
+/* function that sends a fetch request for each value from array argument */
 function showStrokeOrder(array) {
-    for (let i=0; i < array.length; i++) {
-        const url = api_endpoint + array[i];
-        
-        //Pass the request object the url (as for fetch) and the custom setup (myInit)
-        const request = new Request(url, myInit);
-        
-        // Pass request to fetch, not the url
-        fetch(request) 
-            .then(response => response.json())
-            .then(results => {
-            // test results
-                getStrokes(results, i);
-            })
-            .catch(function(error) {
-                console.log(error);
-        });
+    
+    /* Check that the first div from divArray is not null */
+    if (document.querySelector(`[data-name=${CSS.escape(divArray[0])}]`)) {
+        // for each value in array, create a url string
+        for (let i=0; i < array.length; i++) {
+            const url = api_endpoint + array[i];
+
+            //Pass the request object the url (as for fetch) and the custom setup (myInit)
+            const request = new Request(url, myInit);
+
+            // Pass request to fetch, not the url
+            fetch(request) 
+                .then(response => response.json())
+                .then(results => {
+                // get results, pass for loop index value
+                    getStrokes(results, i);
+                })
+                .catch(function(error) {
+                    console.log(error);
+            });
+        }
     }
 }
 
-showStrokeOrder(numberArray);
-
 
 function getStrokes(results, i) {
+    // use i from for loop in ShowStrokeOrder(), to get the right index for divArray
     let imageDiv = document.querySelector(`[data-name=${CSS.escape(divArray[i])}]`);
     
+    // if it is not null
     if (imageDiv) {
         let content = "";
 
+        // loop through the array of strokes, and show each as separate image
         for (let j=0; j < results.kanji.strokes.images.length; j++) {
             content += "<img class='stroke' src='" + results.kanji.strokes.images[j] + "' ";
             content += "alt='Stroke " + (j+1) + " of " + results.kanji.character + "'></img>";
@@ -120,3 +134,14 @@ function getStrokes(results, i) {
         }
     }
 }
+
+
+// when the window finishes loading, load the numbers
+window.addEventListener('load', function() { showStrokeOrder(numberArray) }, false)
+
+
+/**********  *********/
+    /* Kanji Search */
+/*********   *********/
+
+
